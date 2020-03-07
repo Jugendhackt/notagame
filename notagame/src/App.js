1717599c27220d6966470d6945c7793c884e94cf
livgm <livmaertens@gmail.com>
2020-03-07 15:35:04 +0100

    cleaned up react code

--- notagame/src/App.js
+++ notagame/src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Auswahlseite from './Auswahlseite';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Wir programmieren jetzt eine Auswahlseite.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <Auswahlseite/>
     </div>
   );
 }
---
+++ notagame/src/Auswahlseite.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class Auswahlseite extends React.Component {
+  render() {
+    return(<h1>Hello</h1>);
+  }
+}
+
+export default Auswahlseite;
